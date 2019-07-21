f51dd711eeb1ad1e04b1589b9c8a6efcdd7e2842
verapavlovnamsk <cris.glazkowa@yandex.ru>
2019-07-21 20:39:27 +0300

    test

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -36,9 +36,9 @@ export class SliderComponent implements OnInit {
 
   fillDataIcons() {
     this.dataItems = new Array<SliderIconData>();
-    this.dataItems.push(new SliderIconData("Сбп-коннект","P1", "assets/i/one.png", 0));
-    this.dataItems.push(new SliderIconData("Title2","P2", "assets/i/two.png", 1));
-    this.dataItems.push(new SliderIconData("Title3","P3", "assets/i/three.png", 2));
+    this.dataItems.push(new SliderIconData("— Сбп-коннект","Коробочное решение для подключения Банка к Системе Быстрых Платежей, действительно проверенное опытом. В начале 2019 года НСПК  и Центральный банк запустили пилот СБП, в котором приняли участие 12 организаций в т. ч. и наше решение, которое является успешным проектом.", "assets/i/one.png", 0));
+    this.dataItems.push(new SliderIconData("— 3-D Secure v2","Защита Мерчантов, Эмитентов и держателей карт от мошенничества с платежами без присутствия карты (card notpresent или CNP) и стандарт взаимодействия между всеми участниками платежа, позволяя закрепить ответственность за Эмитентом или Эквайером.", "assets/i/two.png", 1));
+    this.dataItems.push(new SliderIconData("— Интернет-эквайринг","Многофункциональное решение для развития интернет эквайринга реализует различные варианты подключения и схемы работы для ТСП: платежная страница, платежный виджет, платежные модули для популярных CSM, API для прямого подключения, 3ds/non3ds, автоподтверждение платежа, выставление счетов, привязка карт, и многое другое.", "assets/i/three.png", 2));
   }
   
   prevImage() {
