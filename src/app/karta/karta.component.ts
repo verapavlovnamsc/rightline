250b28bc531e44264a3e752ae53caf8bfc9c399a
verapavlovnamsk <cris.glazkowa@yandex.ru>
2019-07-30 22:30:10 +0300

    olini oshibki

--- src/app/karta/karta.component.ts
+++ src/app/karta/karta.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-karta',
   templateUrl: './karta.component.html',
   styleUrls: ['./karta.component.css']
 })
+
 export class KartaComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  
 }
+
+
+
